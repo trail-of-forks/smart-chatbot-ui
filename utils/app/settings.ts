8a3ee5dc14ceceecf77888b887e4951c8fd4971a
Shinji Yamada <dotneet@gmail.com>
2023-04-23 16:51:14 +0900

    remove unused code

--- utils/app/settings.ts
+++
@@ -1,7 +0,0 @@
-import { Settings } from '@/types/settings';
-
-const STORAGE_KEY = 'settings';
-
-export const saveSettings = (settings: Settings) => {
-  localStorage.setItem(STORAGE_KEY, JSON.stringify(settings));
-};
