baddb3a05e56d68450347a70270a6c0302cebc6e
Shinji Yamada <dotneet@gmail.com>
2023-04-27 13:21:49 +0900

    fix empty labels

--- components/Chatbar/Chatbar.tsx
+++ components/Chatbar/Chatbar.tsx
@@ -197,7 +197,7 @@ export const Chatbar = () => {
         value: conversations,
       });
     }
-  }, [searchTerm, conversations]);
+  }, [searchTerm, conversations, chatDispatch]);
 
   return (
     <ChatbarContext.Provider
