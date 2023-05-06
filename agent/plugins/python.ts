fe0a7484859c87922be0fbeb97e7d080272d5685
Shinji Yamada <dotneet@gmail.com>
2023-05-07 05:00:40 +0900

    fix python tool prompt

--- agent/plugins/python.ts
+++ agent/plugins/python.ts
@@ -6,20 +6,27 @@ export default {
   nameForModel: 'python_interpreter',
   nameForHuman: 'Python Interpreter',
   descriptionForHuman: 'You can run python code here.',
-  descriptionForModel: `The tool for running python code. The text output to stdout by the program code will be return as as result of this tool. So program should print the results always. If the program is for create an image, write the image to working directory as png file. You can use the third party packages contained in this list ['numpy', 'pandas', 'matprotlib', 'opencv-python', 'requests']. This tool accept the json format input with the property 'code' only. 'code' property contains the python code to be executed. ex. {"code": "print('hello')"}. The code should be a valid python code. The code should print the results always. The code should not contain any infinite loops. The code should not contain any input statements. When the image urls are returned as results, the image should be displayed using markdown notation.`,
+  descriptionForModel: `The tool for running python code. The output text is treated as results of this tool. So program codes should ouput the results to stdout always. If the program codes are for create an image, write the image file to working directory. You can use the third party packages contained in this list ['numpy', 'pandas', 'matprotlib', 'opencv-python', 'requests']. This tool accept the json format input with the property 'code' only. 'code' property contains the python code to be executed. ex. {"code": "print('hello')"}. The code should be a valid python code. The code should print the results always. The code should not contain any infinite loops. The codes should not contain any input statements. When the image urls are returned as the results, the image should be displayed using markdown notation.`,
   displayForUser: true,
   execute: async (
     context: TaskExecutionContext,
     action: Action,
   ): Promise<string> => {
-    const json = JSON.parse(action.pluginInput);
-    const uri = 'http://localhost:8080/api/run';
+    const isJson = action.pluginInput.slice(0, 1) === '{';
+    const uri = process.env.PYTHON_INTERPRETER_BACKEND || '';
+    let code = '';
+    if (isJson) {
+      const json = JSON.parse(action.pluginInput);
+      code = json['code'];
+    } else {
+      code = action.pluginInput;
+    }
     const response = await fetch(uri, {
       method: 'POST',
       headers: {
         'Content-Type': 'application/json',
       },
-      body: JSON.stringify({ code: json['code'] }),
+      body: JSON.stringify({ language: 'python', code }),
     });
     const result = await response.json();
     return JSON.stringify(result);
